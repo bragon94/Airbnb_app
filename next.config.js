585df33f6d7d7bf23ac75b61c4900953f388d28a
BRYAN GONZALEZ <50031443+bragon94@users.noreply.github.com>
2022-03-06 11:30:46 -0500

    first version airbnb clone

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    domains: ["images.pexels.com", "a0.muscache.com", "upload.wikimedia.org"],
+  },
+  env: {
+    mapbox_key: "pk.eyJ1IjoiYnJhZ29uOTQiLCJhIjoiY2t4c2NlYXM0MHVuYTJubnlkMWliYnNhMyJ9.76jqs4iaC3-PasHYOcGCrw",
+  },
+};
